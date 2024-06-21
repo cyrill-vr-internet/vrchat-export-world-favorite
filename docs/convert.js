316f96c91c2958a25930ab513dbe4532d9532877
cyrill.vr.internet <160121879+cyrill-vr-internet@users.noreply.github.com>
2024-06-21 16:05:31 +0900

    tagsが空のケースの考慮漏れ対応

--- docs/convert.js
+++ docs/convert.js
@@ -10,17 +10,17 @@ function onClickConvert(srcTextAreaId, destTextAreaId, logId = null) {
         */
     try {
         document.getElementById(destTextAreaId).value = JSON.parse(document.getElementById(srcTextAreaId).value).map(
-            (w) => 
-                w["tags"].map(
+            (w) =>
+                (w["tags"] ?? []).map(
                     (t) => [
                         `"${w["favoriteId"]}"`,
                         `"${w["id"]}"`,
                         `"${w["name"]}"`,
-                        `"${w["unityPackages"].map((w) => w["platform"]).includes("android") ? "android" : "pc"}"`,
+                        `"${(w["unityPackages"] ?? []).map((w) => w["platform"]).includes("android") ? "android" : "pc"}"`,
                         `"${t}"`
                     ].join("\t")
                 ).join("\n")
-                ).join("\n");
+        ).join("\n");
     } catch (e) {
         if (logId != null) {
             document.getElementById(destTextAreaId).innerText = `エラーが発生しました: ${e}`
