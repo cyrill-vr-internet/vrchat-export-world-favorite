5b15b1edfacf476753ac51ca710d15b7146fc711
cyrill.vr.internet <160121879+cyrill-vr-internet@users.noreply.github.com>
2024-06-21 17:30:08 +0900

    tag不要を追加

--- docs/convert.js
+++ docs/convert.js
@@ -1,37 +1,37 @@
-function onClickConvert(srcTextAreaId, destTextAreaId, logId = null) {
-    /*
-    document.getElementById(destTextAreaId).innerText = JSON.parse(document.getElementById(srcTextAreaId).value).map(
-        (w) => [
-            w["favoriteId"],
-            w["id"],
-            w["name"],
-            w["unityPackages"].map((w) => w["platform"]).includes("android") ? "android" : "pc"
-        ].concat(w["tags"]).map((x) => `"${x}"`).join("\t")).join("\n");
-        */
-    try {
-        document.getElementById(destTextAreaId).value = JSON.parse(document.getElementById(srcTextAreaId).value).map(
-            (w) =>
-                (w["tags"] ?? []).map(
-                    (t) => [
-                        `"${w["favoriteId"]}"`,
-                        `"${w["id"]}"`,
-                        `"${w["name"]}"`,
-                        `"${(w["unityPackages"] ?? []).map((w) => w["platform"]).includes("android") ? "android" : "pc"}"`,
-                        `"${t}"`
-                    ].join("\t")
-                ).join("\n")
-        ).join("\n");
-    } catch (e) {
-        if (logId != null) {
-            document.getElementById(destTextAreaId).innerText = `エラーが発生しました: ${e}`
+function onClickConvert(srcTextAreaId, destTextAreaId, checkBoxId, logId = null) {
+  try {
+    document.getElementById(destTextAreaId).value = JSON.parse(document.getElementById(srcTextAreaId).value).map(
+      (w) => {
+        if (document.getElementById(checkBoxId).checked) {
+          return [
+            `"${w["favoriteId"]}"`,
+            `"${w["id"]}"`,
+            `"${w["name"]}"`,
+            `"${(w["unityPackages"] ?? []).map((w) => w["platform"]).includes("android") ? "android" : "pc"}"`
+          ].join("\t");
+        } else {
+          return (w["tags"] ?? []).map(
+            (t) => [
+              `"${w["favoriteId"]}"`,
+              `"${w["id"]}"`,
+              `"${w["name"]}"`,
+              `"${(w["unityPackages"] ?? []).map((w) => w["platform"]).includes("android") ? "android" : "pc"}"`,
+              `"${t}"`
+            ].join("\t")).join("\n");
         }
+      }
+    ).join("\n");
+  } catch (e) {
+    if (logId != null) {
+      document.getElementById(destTextAreaId).innerText = `エラーが発生しました: ${e}`
     }
+  }
 }
 
 function onClickCopy(srcTextAreaId, logId) {
-    navigator.clipboard.writeText(document.getElementById(srcTextAreaId).value);
-    document.getElementById(logId).innerText = "コピーしました";
-    setTimeout(() => {
-        document.getElementById(logId).innerText = "";
-    }, "3000");
+  navigator.clipboard.writeText(document.getElementById(srcTextAreaId).value);
+  document.getElementById(logId).innerText = "コピーしました";
+  setTimeout(() => {
+    document.getElementById(logId).innerText = "";
+  }, "3000");
 }
